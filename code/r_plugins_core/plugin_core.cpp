01b2bb2ae192f94053b58f759b3535f1abf62df2
lomaka1313 <lomaka13@ukr.net.>
2025-03-03 15:43:24 +0000

    init plugin

---
+++ code/r_plugins_core/plugin_core.cpp
@@ -0,0 +1,268 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+#include <linux/videodev2.h>
+
+#include "../public/ruby_core_plugin.h"
+#include "../public/utils/core_plugins_utils.h"
+
+#define NBUF 3
+
+void query_capabilites(int fd)
+{
+	struct v4l2_capability cap;
+
+	if (-1 == ioctl(fd, VIDIOC_QUERYCAP, &cap)) {
+		perror("Query capabilites");
+		exit(EXIT_FAILURE);
+	}
+
+	if (!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)) {
+		fprintf(stderr, "Device is no video capture device\\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (!(cap.capabilities & V4L2_CAP_READWRITE)) {
+		fprintf(stderr, "Device does not support read i/o\\n");
+	}
+
+	if (!(cap.capabilities & V4L2_CAP_STREAMING)) {
+		fprintf(stderr, "Devices does not support streaming i/o\\n");
+		exit(EXIT_FAILURE);
+	}
+}
+
+int set_format(int fd)
+{
+	struct v4l2_format format = {0};
+	format.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	format.fmt.pix.width = 640;
+	format.fmt.pix.height = 480;
+	format.fmt.pix.pixelformat = V4L2_PIX_FMT_MJPEG;
+	format.fmt.pix.field = V4L2_FIELD_NONE;
+
+	int res = ioctl(fd, VIDIOC_S_FMT, &format);
+	if(res == -1) {
+		perror("Could not set format");
+		exit(EXIT_FAILURE);
+	}
+
+	return res;
+}
+
+int request_buffer(int fd, int count)
+{
+	struct v4l2_requestbuffers req = {0};
+	req.count = count;
+	req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	req.memory = V4L2_MEMORY_MMAP;
+
+	if (ioctl(fd, VIDIOC_REQBUFS, &req) == -1) {
+		perror("Requesting Buffer");
+		exit(EXIT_FAILURE);
+	}
+
+	return req.count;
+}
+
+int query_buffer(int fd, int index, unsigned char **buffer)
+{
+	struct v4l2_buffer buf = {0};
+	buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	buf.memory = V4L2_MEMORY_MMAP;
+	buf.index = index;
+
+	if(ioctl(fd, VIDIOC_QUERYBUF, &buf) == -1) {
+		perror("Could not query buffer");
+		exit(EXIT_FAILURE);
+	}
+
+	*buffer = (u_int8_t*)mmap (NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buf.m.offset);
+	return buf.length;
+}
+
+int queue_buffer(int fd, int index)
+{
+	struct v4l2_buffer bufd = {0};
+	bufd.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	bufd.memory = V4L2_MEMORY_MMAP;
+	bufd.index = index;
+
+	if (ioctl(fd, VIDIOC_QBUF, &bufd) == -1) {
+		perror("Queue Buffer");
+		exit(EXIT_FAILURE);
+	}
+
+	return bufd.bytesused;
+}
+
+int start_streaming(int fd)
+{
+	unsigned int type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	if(ioctl(fd, VIDIOC_STREAMON, &type) == -1) {
+		perror("VIDIOC_STREAMON");
+		exit(EXIT_FAILURE);
+	}
+
+	return 0;
+}
+
+int dequeue_buffer(int fd)
+{
+	struct v4l2_buffer bufd = {0};
+	bufd.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	bufd.memory = V4L2_MEMORY_MMAP;
+	bufd.index = 0;
+
+	if (ioctl(fd, VIDIOC_DQBUF, &bufd) == -1) {
+		perror("DeQueue Buffer");
+		exit(EXIT_FAILURE);
+	}
+
+	return bufd.index;
+}
+
+
+int stop_streaming(int fd)
+{
+	unsigned int type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	if(ioctl(fd, VIDIOC_STREAMOFF, &type) == -1) {
+		perror("VIDIOC_STREAMON");
+		exit(EXIT_FAILURE);
+	}
+
+	return 0;
+}
+
+int main()
+{
+	unsigned char *buffer[NBUF];
+	int fd = open("/dev/video0", O_RDWR);
+	int size;
+	int index;
+	int nbufs;
+
+	/* Step 1: Query capabilities */
+	query_capabilites(fd);
+
+	/* Step 2: Set format */
+	set_format(fd);
+
+	/* Step 3: Request Format */
+	nbufs = request_buffer(fd, NBUF);
+	if ( nbufs > NBUF) {
+		fprintf(stderr, "Increase NBUF to at least %i\n", nbufs);
+		exit(EXIT_FAILURE);
+	}
+
+	for (int i = 0; i < NBUF; i++) {
+		/* Step 4: Query buffers 
+		 * Assume all sizes is equal.
+		 * */
+		size = query_buffer(fd, i, &buffer[i]);
+
+		/* Step 5: Queue buffer */
+		queue_buffer(fd, i);
+	}
+
+	/* Step 6: Start streaming */
+	start_streaming(fd);
+
+	fd_set fds;
+	FD_ZERO(&fds);
+	FD_SET(fd, &fds);
+	struct timeval tv = {0};
+	tv.tv_sec = 2;
+	int r = select(fd+1, &fds, NULL, NULL, &tv);
+	if(-1 == r){
+		perror("Waiting for Frame");
+		exit(1);
+	}
+
+	/* Step 7: Dequeue buffer */
+	index = dequeue_buffer(fd);
+
+	int file = open("output.raw", O_RDWR | O_CREAT, 0666);
+	write(file, buffer[index], size);
+
+	/* Step 8: Stop streaming */
+	stop_streaming(fd);
+
+
+	/* Cleanup the resources */
+	for (int i =0; i < NBUF; i++) {
+		munmap(buffer[i], size);
+	}
+	close(file);
+	close(fd);
+
+	return 0;
+}
+
+const char* g_szPluginNameExample = "Usb camera Core Plugin";
+const char* g_szUIDExample = "4C1D7AE1-71B4-4136-9BAF-CEC92C81A31F";
+
+u32 g_uRuntimeLocation = 0;
+u32 g_uAllocatedCapabilities = 0;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// This is the first method called at runtime, on any runtime location where the plugin is running (vehicle/controller).
+// The plugin should just return the desired capabilities.
+u32 core_plugin_on_requested_capabilities()
+{
+   core_plugin_util_log_line("USB camera plugin request capabilities.");
+
+   return CORE_PLUGIN_CAPABILITY_VIDEO_STREAM;
+}
+
+// The plugin should return a user friendly name for the plugin
+const char* core_plugin_get_name()
+{
+   return g_szPluginNameExample;
+}
+
+// The plugin should return a unique alphanumeric string GUID (i.e. "342-ASDFSA-232-JASKD")
+const char* core_plugin_get_guid()
+{
+   return g_szUIDExample;
+}
+
+// The plugin should return it's current version. It's used by Ruby to manage updates of the plugins and versioning.
+int core_plugin_get_version()
+{
+   return 1;
+}
+
+// This method should be used to actually initialize any data the plugin needs.
+// The actual capabilities allocated by Ruby to the plugin are in the uAllocatedCapabilities parameter.
+// Plugin should return 0 if the initialization succeeded.
+int core_plugin_init(u32 uRuntimeLocation, u32 uAllocatedCapabilities)
+{
+   g_uRuntimeLocation = uRuntimeLocation;
+   g_uAllocatedCapabilities = uAllocatedCapabilities;
+
+   core_plugin_util_log_line("Usb camera plugin init");
+   return 0;
+}
+
+// This is the last method called at runtime, before a plugin is unloaded (due to a reboot or plugin uninstall).
+void core_plugin_uninit()
+{
+   core_plugin_util_log_line("Usb camera plugin uninit");
+}
+
+int core_plugin_on_get_video_stream_source_type() {
+    return CORE_PLUGIN_VIDEO_STREAM_SOURCE_USB;
+}
+
+#ifdef __cplusplus
+}
+#endif
